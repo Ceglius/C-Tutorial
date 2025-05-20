a8e23cea778dc6b4192bf65e35675137178631e1
Ceglius <74723353+Ceglius@users.noreply.github.com>
2025-05-20 16:35:33 +0000

    variable

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿// Console.WriteLine("Hello!");
+// Console.WriteLine("[S]ee all TODOs");
+// Console.WriteLine("[A]dd a TODO");
+// Console.WriteLine("[R]emove a TODO");
+// Console.WriteLine("[E]xit");
+
+string input = Console.ReadLine();
+
+Console.ReadKey();
--- Fundamentals/obj/Debug/net8.0/Fundamentals.AssemblyInfo.cs
+++ Fundamentals/obj/Debug/net8.0/Fundamentals.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fundamentals")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5d72a3c64961d73ebe9688933ce39262679b7a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db03cc86ded2d6a6b342e22a34f68cf0e1cf8559")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fundamentals")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fundamentals")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
